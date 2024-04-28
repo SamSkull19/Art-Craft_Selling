19860df84739b1898876c39e9058bb8bf63a1280
SamSkull19 <sifatsamin19@gmail.com>
2024-04-29 00:06:48 +0600

    router added in navbar

--- src/BasicStructure/Navbar.jsx
+++ src/BasicStructure/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-20 p-2 shadow rounded-box w-52 bg-stone-400 bg-opacity-80">
                             <li><Link to='/'>Home</Link></li>
                             <li><Link to='/'>All Art & craft Items</Link></li>
-                            <li><Link to='/'>Add Craft Item</Link></li>
+                            <li><Link to='/addCraftItems'>Add Craft Item</Link></li>
                             <li><Link to='/'>My Art&Craft List</Link></li>
                             {/* {
                                 user && <>
@@ -43,7 +43,7 @@ const Navbar = () => {
                     <ul className="menu menu-horizontal px-1">
                         <li><Link to='/'>Home</Link></li>
                         <li><Link to='/'>All Art & Craft Items</Link></li>
-                        <li><Link to='/'>Add Craft Item</Link></li>
+                        <li><Link to='/addCraftItems'>Add Craft Item</Link></li>
                         <li><Link to='/'>My Art & Craft List</Link></li>
                         {
                             // user && <>
